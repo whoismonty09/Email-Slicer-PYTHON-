736223d68fdb3e5698b6416c8a35d05e8b817f86
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-02 14:11:55 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
+print("Welcome to Email Slicer developed by Monty")
+
+email = input("Enter your email address:").strip()
+
+if "@" in email and "." in email:
+    username, domain = email.split("@")
+    print("Username:", username)
+    print("Domain:", domain)
+else:
+    print("Invalid email address . Please try again.")    
 
